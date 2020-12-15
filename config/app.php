436d62029a266259ee5ef1d84e5a602616f9a1ac
Le Hoang Tu <mrahn1234@github.com>
2020-12-16 00:32:34 +0700

    modify config

--- config/app.php
+++ config/app.php
@@ -237,8 +237,8 @@ return [
         'Cloudinary' => CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::class,
 
     ],
-    // '_SERVICE_URL' => 'https://similatirydocs.herokuapp.com/',
-    '_SERVICE_URL' => 'http://127.0.0.1:5000/',
+    '_SERVICE_URL' => 'https://similatirydocs.herokuapp.com/',
+    // '_SERVICE_URL' => 'http://127.0.0.1:5000/',
     'STATUS_NEWS' => 1,
     '_PAGINATION_OFFSET' => 9,
     '_TAKE_OFFSET' => 4,
