4bd150e45b2f0812c07ee3d9f42e42077f541613
Hoang Tu <mrahn1234@github.com>
2020-12-18 05:46:23 +0000

    config on server

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('scrape:crawl')->hourly();
+        $schedule->command('scrape:crawl')->everyThirtyMinutes()->withoutOverlapping();
     }
 
     /**
