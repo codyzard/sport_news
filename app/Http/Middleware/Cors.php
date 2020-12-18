a24032cf6326cc0326a778d1dab040bf43e6f330
Le Hoang Tu <mrahn1234@github.com>
2020-12-19 05:49:14 +0700

    fix upload

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,11 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', ' Origin, Content-Type, Accept, Authorization, X-Request-With')
+            ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range')
             ->header('Access-Control-Allow-Credentials', ' true');
+            
     }
+    // ->header('Access-Control-Allow-Headers', ' Origin, Content-Type, Accept, Authorization, X-Request-With')
+    // ->header('Access-Control-Allow-Headers', '*')
+
 }
